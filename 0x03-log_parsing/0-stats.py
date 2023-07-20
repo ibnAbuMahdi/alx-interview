0e5f6e030b6270d26cc4a4ce2e7fb9b7d03e9272
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-20 16:37:54 +0000

    crxn 4 for 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -28,8 +28,12 @@ def isvalid(line):
     if not isinstance(line, str):
         return 0
     parts = line.split()
-    if parts[-1].isdigit() and parts[-2].isdigit():
-        return 1
+    sev = len(parts) == 8 and (not parts[-1].isdigit()
+                               or not parts[-2].isdigit())
+    eit = len(parts) > 8 and (not parts[-2].isdigit()
+                              or not parts[-3].isdigit())
+    if eit or sev:
+        return 0
     ip = parts[0].split(".")
     for n in ip:
         if not n.isdigit():
