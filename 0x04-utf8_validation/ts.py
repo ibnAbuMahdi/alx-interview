0bfa32a56841398cce144fa2aad31183eb22f87a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-27 13:05:11 +0000

    crxn 3 for 0

--- 0x04-utf8_validation/ts.py
+++ 0x04-utf8_validation/ts.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" utf8 validation module """
-
 
 def b22(j, data):
-    """ checks byte value in range """
     return j+1 < len(data) and data[j+1] > 127 \
         and data[j+1] < 192
 
 
 def validUTF8(data):
-    """ checks data is a valid utf8 code """
-    if isinstance(data, list) and len(data) and isinstance(data[0], int):
+    if isinstance(data, list) and len(data):
         ln = len(data)
         j = 0
+
         while j < ln:
-            b1 = data[j] >= 0 and data[j] < 128
+            b1 = data[j] > 0 and data[j] < 128
             b2 = data[j] > 191 and data[j] < 224 and b22(j, data)
             b3 = data[j] > 223 and data[j] < 240 \
                 and b22(j, data) and b22(j+1, data)
@@ -24,13 +21,18 @@ def validUTF8(data):
                 and b22(j+2, data)
             if b1:
                 j += 1
+                continue
             elif b2:
                 j += 2
+                continue
             elif b3:
                 j += 3
+                continue
             elif b4:
                 j += 4
+                continue
             else:
                 return False
         return True
     return False
+
